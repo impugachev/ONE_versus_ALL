77455e2677ea9df903f7f52ea3bcbf17d5e652e0
igor <wotwbb@gmail.com>
2017-09-07 19:33:33 +0300

    In Development

--- main.cpp
+++ main.cpp
@@ -3,4 +3,29 @@
 int main()
 {
   sf::RenderWindow window(sf::VideoMode(800, 600), "ONE versus ALL");
+  window.setFramerateLimit(60);
+  sf::CircleShape hero(40);
+  hero.move(400, 300);
+  while (window.isOpen())
+  {
+    sf::Event event;
+    while (window.pollEvent(event))
+    {
+      if (event.type == sf::Event::Closed)
+        window.close();
+    }
+
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+      hero.move(0, 5);
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+      hero.move(0, -5);
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
+      hero.move(-5, 0);
+    if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
+      hero.move(5, 0);
+    window.clear();
+    window.draw(hero);
+    window.display();
+  }
+  return 0;
 }
