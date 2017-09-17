ab3fdb5d93d17fac34a6fa5ace343d47776a1393
Игорь Пугачев <wotwbb@gmail.com>
2017-09-17 19:35:23 +0300

    Add files via upload

---
+++ cmake-build-debug/CMakeFiles/SFML.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SFML.dir/main.cpp.o"
+  "CMakeFiles/SFML.dir/Living.cpp.o"
+  "CMakeFiles/SFML.dir/Hero.cpp.o"
+  "CMakeFiles/SFML.dir/Zombie.cpp.o"
+  "CMakeFiles/SFML.dir/Weapon.cpp.o"
+  "CMakeFiles/SFML.dir/Bullet.cpp.o"
+  "CMakeFiles/SFML.dir/Soldier.cpp.o"
+  "CMakeFiles/SFML.dir/Game.cpp.o"
+  "SFML.pdb"
+  "SFML"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SFML.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ cmake-build-debug/CMakeFiles/Source.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Source.dir/main.cpp.o"
+  "CMakeFiles/Source.dir/Living.cpp.o"
+  "CMakeFiles/Source.dir/Hero.cpp.o"
+  "CMakeFiles/Source.dir/Monster.cpp.o"
+  "CMakeFiles/Source.dir/Weapon.cpp.o"
+  "CMakeFiles/Source.dir/Bullet.cpp.o"
+  "CMakeFiles/Source.dir/Soldier.cpp.o"
+  "CMakeFiles/Source.dir/Game.cpp.o"
+  "Source.pdb"
+  "Source"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Source.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
